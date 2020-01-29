b4d10d3550bb5534aadaa4d8a1fddc5bb2a8dfd3
angelique-w <angelique.wons@gmail.com>
2020-01-29 23:31:58 +0100

    customize

--- src/HomePage/ProchaineRepresentation.jsx
+++ src/HomePage/ProchaineRepresentation.jsx
@@ -4,32 +4,39 @@ import { Row, Col, Button } from "reactstrap";
 function ProchaineRepresentation() {
     return (
         <div className="min-vh-100 d-flex flex-column justify-content-center align-items-center">
-            <Row className=" bg-secondary flex-row justify-content-center align-items-center w-50">
+            <Row
+                style={{ backgroundColor: "rgb(242, 242, 242, 0.5)" }}
+                className="flex-row justify-content-center align-items-center w-75"
+            >
                 <Col>
-                    <h1 className="text-center m-5 text-light">
+                    <h1 className="text-center m-5 ">
                         Prochaine représentation
                     </h1>
                 </Col>
             </Row>
-            <Row className=" bg-secondary flex-row justify-content-center align-items-center w-50">
+            <Row
+                style={{ backgroundColor: "rgb(242, 242, 242, 0.5)" }}
+                className=" flex-row justify-content-center align-items-center w-75"
+            >
                 <Col xs={12} lg={{ size: 4 }}>
-                    <h3 className="text-center mb-4 text-light">
-                        En ce moment à VILLE
-                    </h3>
-                    <p className="text-center text-light">Du date au date</p>
+                    <h3 className="text-center mb-4 ">En ce moment à VILLE</h3>
+                    <p className="text-center ">Du date au date</p>
                     <hr style={{ width: 200 }} />
-                    <p className="text-center text-light"> LIEU</p>
+                    <p className="text-center "> LIEU</p>
                 </Col>
                 <Col xs={12} lg={{ size: 4 }}>
-                    <h3 className="text-center mb-4 text-light">
+                    <h3 className="text-center mb-4 ">
                         Prochaine représentation
                     </h3>
-                    <p className="text-center text-light">Le date</p>
+                    <p className="text-center ">Le date</p>
                     <hr style={{ width: 200 }} />
-                    <p className="text-center text-light"> LIEU</p>
+                    <p className="text-center "> LIEU</p>
                 </Col>
             </Row>
-            <Row className="bg-secondary flex-row justify-content-center align-items-center w-50 pb-5">
+            <Row
+                style={{ backgroundColor: "rgb(242, 242, 242, 0.5)" }}
+                className="flex-row justify-content-center align-items-center w-75 pb-5"
+            >
                 <Col xs={12} lg={{ size: 4 }}>
                     <div className="text-lg-right text-center my-2">
                         <Button color="primary" size="lg">
