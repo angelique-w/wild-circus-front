e5c079cb54872632acee0bf2d753835878cc3e34
angelique-w <angelique.wons@gmail.com>
2020-01-29 22:52:11 +0100

    customize prochaine representation

--- src/HomePage/ProchaineRepresentation.jsx
+++ src/HomePage/ProchaineRepresentation.jsx
@@ -3,41 +3,43 @@ import { Row, Col, Button } from "reactstrap";
 
 function ProchaineRepresentation() {
     return (
-        <div className="min-vh-100 d-flex flex-column justify-content-around align-items-center">
-            <Row className="flex-row justify-content-center align-items-center w-100">
+        <div className="min-vh-100 d-flex flex-column justify-content-center align-items-center">
+            <Row className=" bg-secondary flex-row justify-content-center align-items-center w-50">
                 <Col>
-                    <h1 className="text-center pt-5">
+                    <h1 className="text-center m-5 text-light">
                         Prochaine représentation
                     </h1>
                 </Col>
             </Row>
-            <Row className="flex-row justify-content-center align-items-center w-100">
+            <Row className=" bg-secondary flex-row justify-content-center align-items-center w-50">
                 <Col xs={12} lg={{ size: 4 }}>
-                    <h3 className="text-center mb-5">En ce moment à VILLE</h3>
-                    <p className="text-center">Du date au date</p>
+                    <h3 className="text-center mb-4 text-light">
+                        En ce moment à VILLE
+                    </h3>
+                    <p className="text-center text-light">Du date au date</p>
                     <hr style={{ width: 200 }} />
-                    <p className="text-center"> LIEU</p>
+                    <p className="text-center text-light"> LIEU</p>
                 </Col>
                 <Col xs={12} lg={{ size: 4 }}>
-                    <h3 className="text-center mb-5">
+                    <h3 className="text-center mb-4 text-light">
                         Prochaine représentation
                     </h3>
-                    <p className="text-center">Le date</p>
+                    <p className="text-center text-light">Le date</p>
                     <hr style={{ width: 200 }} />
-                    <p className="text-center"> LIEU</p>
+                    <p className="text-center text-light"> LIEU</p>
                 </Col>
             </Row>
-            <Row className="flex-row justify-content-center align-items-center w-100">
+            <Row className="bg-secondary flex-row justify-content-center align-items-center w-50 pb-5">
                 <Col xs={12} lg={{ size: 4 }}>
                     <div className="text-lg-right text-center my-2">
-                        <Button color="info" size="lg">
+                        <Button color="primary" size="lg">
                             Réservez vos places
                         </Button>
                     </div>
                 </Col>
                 <Col xs={12} lg={{ size: 4 }}>
                     <div className="text-lg-left text-center my-2">
-                        <Button color="info" size="lg">
+                        <Button color="danger" size="lg">
                             Voir toute la tournée
                         </Button>
                     </div>
